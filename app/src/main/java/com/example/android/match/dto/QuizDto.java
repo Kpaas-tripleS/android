f13cb406781b5208d0ec189691fa04a71f991692
ns-gyu <gyubin601@gmail.com>
2024-09-30 23:53:23 +0900

    feat: #1 match 관련 dto 수정

--- app/src/main/java/com/example/android/match/dto/QuizDto.java
+++ app/src/main/java/com/example/android/match/dto/QuizDto.java
@@ -2,6 +2,7 @@ package com.example.android.match.dto;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 public class QuizDto implements Parcelable {
@@ -24,6 +25,22 @@ public class QuizDto implements Parcelable {
     @SerializedName("choiceFour")
     private String choiceFour;
 
+    @SerializedName("answer")
+    private String answer;
+
+    @SerializedName("isSolved")
+    private Boolean isSolved;
+
+    @SerializedName("incorrectCount")
+    private Integer incorrectCount;
+
+    @SerializedName("level")
+    private DifficultyLevel level;
+
+    public enum DifficultyLevel {
+        BEGINNER, INTERMEDIATE, ADVANCED, RANDOM
+    }
+
     // 기본 생성자
     public QuizDto() {}
 
@@ -39,6 +56,16 @@ public class QuizDto implements Parcelable {
         choiceTwo = in.readString();
         choiceThree = in.readString();
         choiceFour = in.readString();
+        answer = in.readString();
+        byte tmpIsSolved = in.readByte();
+        isSolved = tmpIsSolved == 0 ? null : tmpIsSolved == 1;
+        if (in.readByte() == 0) {
+            incorrectCount = null;
+        } else {
+            incorrectCount = in.readInt();
+        }
+        String levelString = in.readString();
+        level = levelString == null ? null : DifficultyLevel.valueOf(levelString);
     }
 
     public static final Creator<QuizDto> CREATOR = new Creator<QuizDto>() {
@@ -71,6 +98,15 @@ public class QuizDto implements Parcelable {
         parcel.writeString(choiceTwo);
         parcel.writeString(choiceThree);
         parcel.writeString(choiceFour);
+        parcel.writeString(answer);
+        parcel.writeByte((byte) (isSolved == null ? 0 : isSolved ? 1 : 2));
+        if (incorrectCount == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeInt(incorrectCount);
+        }
+        parcel.writeString(level == null ? null : level.name());
     }
 
     // Getters and setters
@@ -121,81 +157,36 @@ public class QuizDto implements Parcelable {
     public void setChoiceFour(String choiceFour) {
         this.choiceFour = choiceFour;
     }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Boolean getIsSolved() {
+        return isSolved;
+    }
+
+    public void setIsSolved(Boolean isSolved) {
+        this.isSolved = isSolved;
+    }
+
+    public Integer getIncorrectCount() {
+        return incorrectCount;
+    }
+
+    public void setIncorrectCount(Integer incorrectCount) {
+        this.incorrectCount = incorrectCount;
+    }
+
+    public DifficultyLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(DifficultyLevel level) {
+        this.level = level;
+    }
 }
-
-
-
-/*package com.example.android.match.dto;
-
-import com.google.gson.annotations.SerializedName;
-
-public class QuizDto {
-
-    @SerializedName("quizId")
-    private Long quizId;
-
-    @SerializedName("question")
-    private String question;
-
-    @SerializedName("choiceOne")
-    private String choiceOne;
-
-    @SerializedName("choiceTwo")
-    private String choiceTwo;
-
-    @SerializedName("choiceThree")
-    private String choiceThree;
-
-    @SerializedName("choiceFour")
-    private String choiceFour;
-
-    public Long getQuizId() {
-        return quizId;
-    }
-
-    public void setQuizId(Long quizId) {
-        this.quizId = quizId;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getChoiceOne() {
-        return choiceOne;
-    }
-
-    public void setChoiceOne(String choiceOne) {
-        this.choiceOne = choiceOne;
-    }
-
-    public String getChoiceTwo() {
-        return choiceTwo;
-    }
-
-    public void setChoiceTwo(String choiceTwo) {
-        this.choiceTwo = choiceTwo;
-    }
-
-    public String getChoiceThree() {
-        return choiceThree;
-    }
-
-    public void setChoiceThree(String choiceThree) {
-        this.choiceThree = choiceThree;
-    }
-
-    public String getChoiceFour() {
-        return choiceFour;
-    }
-
-    public void setChoiceFour(String choiceFour) {
-        this.choiceFour = choiceFour;
-    }
-
-}*/
-
